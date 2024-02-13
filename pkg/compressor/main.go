4649ad8f9f6e10061ee1c46406884027de708408
Dmitriy Tarasenko <dmitriy.tarasenko@dmxlol.io>
2024-02-13 22:17:44 +0200

    feat(compressor): add change brotli compressor

--- pkg/compressor/main.go
+++ pkg/compressor/main.go
@@ -1,10 +1,12 @@
-package compress
+package compressor
 
 import (
 	"bytes"
 	"compress/flate"
 	"compress/gzip"
-	"github.com/dsnet/compress/bzip2"
+	"context"
+	"github.com/andybalholm/brotli"
+	"github.com/dimixlol/hosts-proxy/logging"
 	"io"
 )
 
@@ -19,8 +21,7 @@ type (
 		Compress(encoding string, body string) []byte
 		Decompress(encoding string, body io.ReadCloser) string
 	}
-
-	compressor struct{}
+	compressor struct{ ctx context.Context }
 )
 
 func compress(data []byte, encoder io.WriteCloser) {
@@ -42,11 +43,7 @@ func (c *compressor) Compress(encoding string, data string) []byte {
 	case Gzip:
 		compress(dataBytes, gzip.NewWriter(&buffer))
 	case Brotli:
-		encoder, err := bzip2.NewWriter(&buffer, nil)
-		if err != nil {
-			panic(err)
-		}
-		compress(dataBytes, encoder)
+		compress(dataBytes, brotli.NewWriter(&buffer))
 	case Deflate:
 		encoder, err := flate.NewWriter(&buffer, flate.BestCompression)
 		if err != nil {
@@ -61,9 +58,10 @@ func (c *compressor) Compress(encoding string, data string) []byte {
 }
 
 func (c *compressor) Decompress(encoding string, body io.ReadCloser) string {
-	var reader io.Reader
-	var err error
-
+	var (
+		reader io.Reader
+		err    error
+	)
 	switch encoding {
 	case Gzip:
 		reader, err = gzip.NewReader(body)
@@ -71,10 +69,7 @@ func (c *compressor) Decompress(encoding string, body io.ReadCloser) string {
 			panic(err)
 		}
 	case Brotli:
-		reader, err = bzip2.NewReader(body, nil)
-		if err != nil {
-			panic(err)
-		}
+		reader = brotli.NewReader(body)
 	case Deflate:
 		reader = flate.NewReader(body)
 	default:
@@ -82,11 +77,11 @@ func (c *compressor) Decompress(encoding string, body io.ReadCloser) string {
 	}
 	decompressed, err := io.ReadAll(reader)
 	if err != nil {
-		panic(err)
+		logging.GetLogger(c.ctx).Errorf(c.ctx, "error while decompressing: %s", err)
 	}
 	return string(decompressed)
 }
 
-func NewCompressor() Compressor {
-	return &compressor{}
+func New(ctx context.Context) Compressor {
+	return &compressor{ctx: ctx}
 }
